65c43d25ea76451914fc3be5f8574deb61111297
Pablo Daza <aketza.daza@me.com>
2019-10-02 00:04:13 -0400

    - first project scafold - ORM configuration

---
+++ Code/Dependencies/CookBook.Data/Context/DomainContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CookBook.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookBook.Data.Context
+{
+    public class DomainContext : DbContext
+    {
+        public DomainContext(DbContextOptions<DomainContext> options) : base(options)
+        {
+        }
+
+        
+
+        public DbSet<Publisher> Publishers { get; set; }
+    }
+}
---
+++ Tests/CookBook.Data.Tests/DatabaseScenarioTests.cs
@@ -0,0 +1,49 @@
+using System;
+using CookBook.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace CookBook.Data.Tests
+{
+    /**
+     *   Here is all the data sources that we can configured
+     *   https://docs.microsoft.com/en-us/ef/core/providers/index
+     */
+    public class DatabaseScenarioTests
+    {
+        private DbContextOptionsBuilder<DomainContext> configureContext()
+        {
+            var options = new DbContextOptionsBuilder<DomainContext>();
+            options.UseInMemoryDatabase("CookBookTest");
+            return options;
+        }
+
+        [Fact]
+        public void CanCreateDatabase()
+        {
+            using (var db = new DomainContext(configureContext().Options))
+            {
+                bool isCreated = db.Database.EnsureCreated();
+                Assert.True(isCreated);
+            }
+        }
+
+        [Fact]
+        public void CanNotCreateDatabaseIfNotConfigured()
+        {
+            using (var db = new DomainContext(new DbContextOptionsBuilder<DomainContext>().Options))
+            {
+                bool isCreated = false;
+                try
+                {
+                    isCreated = db.Database.EnsureCreated();
+                }
+                catch(Exception ex)
+                {
+                    isCreated = false;
+                }
+                Assert.False(isCreated);
+            }
+        }
+    }
+}
